7198b5f81a44ac8940de9b61f4e8e59cd92844b3
Kyle Meyer <kylem@metrumrg.com>
2026-08-21 09:10:24 -0400

    don't run finalizer at end of session
    Call reg.finalizer with the default onexit=FALSE.  All the temporary
    files will get cleaned up at the end of the session when R removes the
    temporary directory.
    
    Suggested-by: Kyle Baron <kyleb@metrumrg.com>

--- R/utils.R
+++ R/utils.R
@@ -132,6 +132,6 @@ get_nid_from_ds <- function(x, nid = 10, batch_size = 10000) {
 }
 
 set_finalizer_ds <- function(x) {
-  reg.finalizer(x, clean_up_ds, onexit = TRUE)
+  reg.finalizer(x, clean_up_ds)
   x
 }
